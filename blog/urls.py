abf2d7e899385ee9ef1c94a86602e791d67689ba
Guilarai <gperelman29@gmail.com>
2024-01-23 14:55:53 +0100

    Modified templates to display posts from database.

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 12:05:17 2024
+
+@author: p1516551
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
